8753f388db51ab1709f18b947fee648fcca92053
bobob0311 <142088813+bobob0311@users.noreply.github.com>
2024-11-06 14:31:18 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int temp = (int) Math.sqrt(n);
+        if(temp*temp == n){
+            return 1;
+        }
+        return 2;
+    }
+}
