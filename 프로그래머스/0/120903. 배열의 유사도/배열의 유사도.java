fcf640cd035cb1ffdcc5ac85e13d5d910d99df66
bobob0311 <142088813+bobob0311@users.noreply.github.com>
2024-11-13 19:34:22 +0900

    [level 0] Title: 배열의 유사도, Time: 0.07 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        Set<String> set = new HashSet<>();
+        for(String s : s1){
+            set.add(s);
+        }
+        for(String s:s2){
+            set.add(s);
+        }
+        
+        return s1.length+s2.length-set.size();
+    }
+}
