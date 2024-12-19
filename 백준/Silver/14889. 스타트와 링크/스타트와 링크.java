f99dc50028485ebee9fa4cde0265bd1c46d92dc0
bobob0311 <142088813+bobob0311@users.noreply.github.com>
2024-12-19 15:22:34 +0900

    [Silver I] Title: 스타트와 링크, Time: 444 ms, Memory: 18996 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static boolean[] chk;
+    static int[][] map;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        chk = new boolean[N];
+        map = new int[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(N, 0, 0);
+        System.out.println(min);
+    }
+
+    public static void dfs(int N, int idx, int cnt) {
+        if (cnt == N / 2) {
+            min = Math.min(min, calc(N));
+            return;
+        }
+        if (idx >= N)
+            return; 
+
+
+        chk[idx] = true;
+        dfs(N, idx + 1, cnt + 1);
+
+
+        chk[idx] = false;
+        dfs(N, idx + 1, cnt);
+    }
+
+    public static int calc(int N) {
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (chk[i] && chk[j]) {
+                    sum1 += map[i][j] + map[j][i];
+                } else if (!chk[i] && !chk[j]) {
+                    sum2 += map[i][j] + map[j][i];
+                }
+            }
+        }
+        return Math.abs(sum1 - sum2);
+    }
+}
