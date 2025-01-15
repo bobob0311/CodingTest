e7137c67c9dd4eea439addf12e61ed863e82dee4
bobob0311 <142088813+bobob0311@users.noreply.github.com>
2025-01-15 19:49:02 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 328 ms, Memory: 38384 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Queue<int[]> que = new LinkedList<>();
+        Stack<Integer> stk = new Stack<>();
+
+        int start = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        que.add(new int[] { start, 0 });
+
+        int[] answer = new int[150001];
+        boolean[] chk = new boolean[150001];
+
+        while (!que.isEmpty()) {
+            int[] nowArr = que.poll();
+            int now = nowArr[0];
+            if (now == target) {
+                System.out.println(nowArr[1]);
+                break;
+            }
+            int nowCnt = nowArr[1];
+            chk[now] = true;
+            if (now - 1 >= 0 && !chk[now - 1]) {
+                chk[now - 1] = true;
+                que.add(new int[] { now - 1, nowCnt + 1 });
+                answer[now - 1] = now;
+            }
+            if (now + 1 <= 150000 && !chk[now + 1]) {
+                chk[now + 1] = true;
+                que.add(new int[] { now + 1, nowCnt + 1 });
+                answer[now + 1] = now;
+            }
+            if (now * 2 <= 150000 && !chk[now * 2]) {
+                chk[now * 2] = true;
+                que.add(new int[] { now * 2, nowCnt + 1 });
+                answer[now * 2] = now;
+            }
+        }
+        int temp = target;
+        while (temp != start) {
+            stk.add(temp);
+            temp = answer[temp];
+        }
+        stk.add(start);
+        StringBuilder sb = new StringBuilder();
+        while (!stk.isEmpty()) {
+            sb.append(stk.pop() + " ");
+        }
+        System.out.println(sb.toString());
+    }
+}
