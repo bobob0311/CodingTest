f4babed3c293f97978b3562e4dc3403ad85b99ad
bobob0311 <142088813+bobob0311@users.noreply.github.com>
2025-10-28 21:06:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+       public static void main(String[] args) throws IOException{
+           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           long answer = 0;
+           
+           for(int i = 0; i< 3; i++){
+               long nowNum = Long.parseLong(st.nextToken());
+               answer += nowNum;
+           }
+           
+           System.out.println(answer);
+       }
+    
+}
