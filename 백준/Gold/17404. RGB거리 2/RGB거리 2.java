816d0428bb654e69b2b178b71c3b4aad93b47c6b
bobob0311 <142088813+bobob0311@users.noreply.github.com>
2026-02-19 20:24:05 +0900

    [Gold IV] Title: RGB거리 2, Time: 116 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -36,62 +36,32 @@ public class Main {
             }
         }
 
-        //N번째 집을 3가지 색으로 칠했을때 최소값을 저장해야한다.
-        // 처음 시작한걸 마지막에 저장해야할듯?
+        int INF = Integer.MAX_VALUE / 2;
+        int answer = INF;
 
-        dp = new int[N][3][3];
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<3; j++){
-                Arrays.fill(dp[i][j], Integer.MAX_VALUE/2);
+        for (int firstColor = 0; firstColor < 3; firstColor++) {
+            int[][] dp = new int[N][3];
+            for (int c = 0; c < 3; c++) {
+                if (c == firstColor) dp[0][c] = costs[0][c];
+                else dp[0][c] = INF;
             }
-        }
 
-        for(int i = 0; i<3; i++){
-            dp[0][i][i] = costs[0][i];
-        }
-
-
-
-        for(int i = 1; i<N; i++){
-            if(i == N-1){
-                for(int j = 0; j<3; j++){
-                    if(j==0){
-                        dp[i][j][1] = Math.min(dp[i-1][1][1],dp[i-1][2][1]) + costs[i][j];
-                        dp[i][j][2] = Math.min(dp[i-1][1][2],dp[i-1][2][2])+ costs[i][j];
-                    }else if(j ==1){
-                        dp[i][j][0] = Math.min(dp[i-1][0][0],dp[i-1][2][0])+ costs[i][j];
-                        dp[i][j][2] = Math.min(dp[i-1][0][2],dp[i-1][2][2])+ costs[i][j];
-                    }else{
-                        dp[i][j][0] = Math.min(dp[i-1][1][0],dp[i-1][0][0])+ costs[i][j];
-                        dp[i][j][1] = Math.min(dp[i-1][1][1],dp[i-1][0][1])+ costs[i][j];
-                    }
-
-                }
-            }else{
-
-
-
-            for(int j = 0; j<3; j++){
-                for(int k = 0; k<3; k++){
-                    if(k == 0){
-                        dp[i][k][j] = Math.min(dp[i-1][1][j], dp[i-1][2][j]) + costs[i][k];
-                    }else if(k == 1){
-                        dp[i][k][j] = Math.min(dp[i-1][0][j], dp[i-1][2][j]) + costs[i][k];
-                    }else{
-                        dp[i][k][j] = Math.min(dp[i-1][0][j], dp[i-1][1][j]) + costs[i][k];
+            for (int i = 1; i < N; i++) {
+                for (int cur = 0; cur < 3; cur++) {
+                    dp[i][cur] = INF;
+                    for (int prev = 0; prev < 3; prev++) {
+                        if (prev == cur) continue; 
+                        dp[i][cur] = Math.min(dp[i][cur], dp[i - 1][prev] + costs[i][cur]);
                     }
                 }
-
             }
+
+            for (int lastColor = 0; lastColor < 3; lastColor++) {
+                if (lastColor == firstColor) continue;
+                answer = Math.min(answer, dp[N - 1][lastColor]);
             }
         }
 
-        int answer = Integer.MAX_VALUE;
-        for(int i = 0; i<3; i++){
-            for(int j = 0; j<3; j++){
-                answer = Math.min(answer, dp[N-1][i][j]);
-            }
-        }
         System.out.println(answer);
     }
 
